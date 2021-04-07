062eb07156ae766464aef087b3cebb386b0fe75a
MichaelDimmitt <michaelgdimmitt@gmail.com>
2021-04-07 00:38:08 -0400

    add loopWorker to count to a billion with workerize-loader

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,12 +1,24 @@
 import './App.css';
-
+import loopWorker from 'workerize-loader!../../workers/loopWorker'; // eslint-disable-line import/no-webpack-loader-syntax
 function App() {
+  function triggerLoopWorker(e) {
+    e.preventDefault()
+    let instance = new loopWorker();
+    instance.countToBillion()
+      .then(x => {
+        console.log({x});
+        return x;
+      })
+      .then(_ => instance.terminate())
+      .catch(_ => instance.terminate());
+  }
   return (
     <div className="App">
       <header >
         <a
+          href="/"
           className="App-link"
-          href="https://reactjs.org"
+          onClick={triggerLoopWorker}
           target="_blank"
           rel="noopener noreferrer"
         >
---
+++ src/workers/loopWorker.js
@@ -0,0 +1,5 @@
+export function countToBillion() {
+    let i = 0;
+    for (i; i < 1000000000; i++) {}
+    return ('finished ' + i)
+};
